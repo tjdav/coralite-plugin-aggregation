065ff1a884204bb1eab67a341796a6b5ad666bcb
Thomas David <tom@mightysmile.com>
2025-06-17 10:24:56 +0200

    feat(pagination): improve pagination handling with dynamic path and adjusted length calculation

--- src/index.js
+++ src/index.js
@@ -147,7 +147,7 @@ export default createPlugin({
       }
     }
 
-    // Apply page limit
+    // apply page limit
     let limit
     if (options.limit) {
       limit = options.limit
@@ -163,12 +163,14 @@ export default createPlugin({
       }
     }
 
+    let pageLength = pages.length
     for (let i = startIndex; i < endIndex; i++) {
       let page = pages[i]
 
       if (page.path.filename === context.document.path.filename) {
         // skip to next page
         page = pages[++i]
+        pageLength--
 
         if (!page) {
           // exit loop
@@ -192,8 +194,8 @@ export default createPlugin({
 
     if (options.pagination) {
       const pagination = options.pagination
-      const paginationInfix = pagination.path || 'page'
-      const paginationLength = Math.floor(pages.length / limit)
+      const paginationPath = context.values.pagination_path || pagination.path || 'page'
+      const paginationLength = Math.floor(pageLength / limit)
       let processed = context.values.pagination_processed
 
       if (!processed && paginationLength) {
@@ -210,45 +212,64 @@ export default createPlugin({
           name = ''
         }
 
-        const dirname = join(context.document.path.dirname, name, paginationInfix)
-        const indexPage = this.pages.getItem(context.document.path.pathname)
+        // @ts-ignore
+        let dirname = join(path.dirname, name, paginationPath)
+        let pageIndex = path.pathname
+        const indexPage = this.pages.getItem(path.pathname)
+        const maxVisiblePages = (pagination.visible || paginationLength).toString()
 
-        context.values = { 
-          ...context.values,
-          pagination_processed: 'true',
-          pagination_offset: endIndex.toString(),
-          pagination_index: path.pathname,
-          pagination_dirname: dirname,
-          pagination_pathname: path.pathname,
-          pagination_length: paginationLength.toString(),
-          pagination_current: path.filename
+        // check if we are currently not on a pager page
+        if (context.values.pagination_pager_dirname == null) {
+          for (let i = 1; i < paginationLength; i++) {
+            const currentPageIndex = i + 1
+            const filename = currentPageIndex + '.html'
+            const pathname = join(dirname, filename)
+            const contextId = pathname + context.id.substring(path.pathname.length)
+            
+            // store global values for pagination page
+            this.values[contextId] = {
+              pagination_path: paginationPath,
+              pagination_visible: maxVisiblePages,
+              pagination_processed: 'true',
+              pagination_offset: (endIndex * i).toString(),
+              pagination_index: path.pathname,
+              pagination_dirname: dirname,
+              pagination_length: paginationLength.toString(),
+              pagination_current: currentPageIndex.toString(),
+            }
+
+            // add pagination page to render queue
+            this.addRenderQueue({
+              values: {
+                pagination_pager_dirname: path.dirname,
+                pagination_pager_index: pageIndex,
+              },
+              path: {
+                dirname,
+                pathname,
+                filename
+              },
+              content: indexPage.content
+            })
+          }
+        } else {
+          // @ts-ignore
+          dirname = join(context.values.pagination_pager_dirname, paginationPath)
+          // @ts-ignore
+          pageIndex = context.values.pagination_pager_index
         }
 
-        for (let i = 0; i < paginationLength; i++) {
-          const filename = i + 2 + '.html'
-          const pathname = join(dirname, filename)
-          // const root = parsePagination(page.content)
-          // const content = this._render(root)
-          // const index = context.instanceId.lastIndexOf(templateId)
-          const contextId = pathname + context.id.substring(context.document.path.pathname.length)
-          this.values[contextId] = { 
-            pagination_processed: 'true',
-            pagination_offset: (endIndex * (i + 1)).toString(),
-            pagination_index: path.pathname,
-            pagination_dirname: dirname,
-            pagination_pathname: pathname,
-            pagination_length: paginationLength.toString(),
-            pagination_current: i.toString(),
-          }
-
-          this.addRenderQueue({
-            path: {
-              dirname,
-              pathname,
-              filename
-            },
-            content: indexPage.content
-          })
+        // store pagination values for current page
+        context.values = { 
+          ...context.values,
+          pagination_path: paginationPath,
+          pagination_visible: maxVisiblePages,
+          pagination_processed: 'true',
+          pagination_offset: endIndex.toString(),
+          pagination_index: pageIndex,
+          pagination_dirname: dirname,
+          pagination_length: paginationLength.toString(),
+          pagination_current: '1'
         }
       }
 
