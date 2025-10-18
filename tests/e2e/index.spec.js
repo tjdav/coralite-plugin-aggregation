1510b030ae69971163d701d1ec439c3be0296d2f
Thomas David <tom@mightysmile.com>
2025-10-18 14:13:51 +0200

    fix(ui): update aria snapshot for paginated blog content and adjust test text match

--- tests/e2e/index.spec.js
+++ tests/e2e/index.spec.js
@@ -1,39 +1,31 @@
 import { test, expect } from '@playwright/test'
 
-test('displays aggregated content excluding it\'s self', async ({ page }) => {
+test('it displays aggregated content excluding it\'s self', async ({ page }) => {
   await page.goto('/blog/index.html')
 
-  await expect(page.locator('body')).toMatchAriaSnapshot(`- main:
-  - heading "Blog posts (excluding this page)" [level=1]
-  - heading "Great Barrier Reef" [level=2]
-  - text: Nemo
-  - time: Wed, 8 Jan 25
-  - img "Photo of a cat"
-  - text: The Great Barrier Reef—largest, comprising over 2,900 individual reefs and 900 islands stretching for over 2,600 kilometers This is the mighty header Dev mode enabled
-  - heading "Mesoamerican Barrier Reef System" [level=2]
-  - text: Nemo
-  - time: Thu, 9 Jan 25
-  - img "Photo of a dog"
-  - text: The Mesoamerican Barrier Reef System—second largest, stretching 1,000 kilometers This is the mighty header Dev mode enabled
-  - heading "New Caledonia Barrier Reef" [level=2]
-  - text: Nemo
-  - time: Thu, 9 Jan 25
-  - img "Photo of a dog"
-  - text: The New Caledonia Barrier Reef—second longest double barrier reef, covering 1,500 kilometers This is the mighty header Dev mode enabled`)
-})
-
-test('has named slots within head', async ({ page }) => {
-  await page.goto('/index.html')
-
-  const metaName = page.locator('meta[name="name"]')
-  const metaDescription = page.locator('meta[name="description"]')
-  const metaDefaultHello = page.locator('meta[name="default-hello"]')
-  const metaDefaultWorld = page.locator('meta[name="default-world"]')
-
-  // named slots
-  await expect(metaName).toHaveAttribute('content', 'coralite')
-  await expect(metaDescription).toHaveAttribute('content', 'look mum, no database!')
-  // default slots
-  await expect(metaDefaultHello).toHaveAttribute('content', 'hello')
-  await expect(metaDefaultWorld).toHaveAttribute('content', 'world')
+  await expect(page.getByText('Paginated blog posts Great')).toMatchAriaSnapshot(`- heading "Paginated blog posts" [level=2]
+- link "Great Barrier Reef":
+  - /url: ""
+  - heading "Great Barrier Reef" [level=3]
+- text: Nemo
+- time: Wed, 8 Jan 25
+- text: The Great Barrier Reef—largest, comprising over 2,900 individual reefs and 900 islands stretching for over 2,600 kilometers
+- list:
+  - listitem: Previous
+  - listitem:
+    - link "1":
+      - /url: /blog/index.html
+  - listitem:
+    - link "2":
+      - /url: /blog/page/2.html
+  - listitem:
+    - link "3":
+      - /url: /blog/page/3.html
+  - listitem: ...
+  - listitem:
+    - link "5":
+      - /url: /blog/page/5.html
+  - listitem:
+    - link "Next":
+      - /url: /blog/page/2.html`)
 })
